dc45295294b0e8b5d1e762ffde10a5603bb36cf5
Alexander Bessman <alexander.bessman@scania.com>
2020-06-25 19:50:49 +0200

    Fix style and remove dead code in AnalogInputSource

--- PSL/achan.py
+++ PSL/achan.py
@@ -1,196 +1,214 @@
-from __future__ import print_function
+from math import isclose
+
 import numpy as np
 
-gains = [1, 2, 4, 5, 8, 10, 16, 32, 1 / 11.]
+GAIN_VALUES = (1, 2, 4, 5, 8, 10, 16, 32, 1 / 11)
+gains = GAIN_VALUES  # Backwards compatibility
 
-# -----------------------Classes for input sources----------------------
-allAnalogChannels = ['CH1', 'CH2', 'CH3', 'MIC', 'CAP', 'SEN', 'AN8']
+ALL_ANALOG_CHANNELS = (
+    "CH1",
+    "CH2",
+    "CH3",
+    "MIC",
+    "CAP",
+    "SEN",
+    "AN8",
+)
+allAnalogChannels = ALL_ANALOG_CHANNELS  # Backwards compatibility
 
-bipolars = ['CH1', 'CH2', 'CH3', 'MIC']
+INPUT_RANGES = {
+    "CH1": (16.5, -16.5),  # Specify inverted channels explicitly by reversing range!
+    "CH2": (16.5, -16.5),
+    "CH3": (-3.3, 3.3),  # external gain control analog input
+    "MIC": (-3.3, 3.3),  # connected to MIC amplifier
+    "CAP": (0, 3.3),
+    "SEN": (0, 3.3),
+    "AN8": (0, 3.3),
+}
 
-inputRanges = {'CH1': [16.5, -16.5],  # Specify inverted channels explicitly by reversing range!!!!!!!!!
-               'CH2': [16.5, -16.5],
-               'CH3': [-3.3, 3.3],  # external gain control analog input
-               'MIC': [-3.3, 3.3],  # connected to MIC amplifier
-               'CAP': [0, 3.3],
-               'SEN': [0, 3.3],
-               'AN8': [0, 3.3]
-               }
+PIC_ADC_MULTIPLEX = {
+    "CH1": 3,
+    "CH2": 0,
+    "CH3": 1,
+    "MIC": 2,
+    "AN4": 4,
+    "SEN": 7,
+    "CAP": 5,
+    "AN8": 8,
+}
 
-picADCMultiplex = {'CH1': 3, 'CH2': 0, 'CH3': 1, 'MIC': 2, 'AN4': 4, 'SEN': 7, 'CAP': 5, 'AN8': 8, }
+RESOLUTION_10BIT = 2 ** 10 - 1
+RESOLUTION_12BIT = 2 ** 12 - 1
 
 
-class analogInputSource:
-    gain_values = gains
-    gainEnabled = False
-    gain = None
-    gainPGA = None
-    inverted = False
-    inversion = 1.
-    calPoly10 = np.poly1d([0, 3.3 / 1023, 0.])
-    calPoly12 = np.poly1d([0, 3.3 / 4095, 0.])
-    calibrationReady = False
-    defaultOffsetCode = 0
+class AnalogInputSource:
+    def __init__(self, name, **kwargs):
+        self.name = name  # The generic name of the input. like 'CH1', 'IN1' etc.
 
-    def __init__(self, name, **args):
-        self.name = name  # The generic name of the input. like 'CH1', 'IN1' etc
-        self.CHOSA = picADCMultiplex[self.name]
-        self.adc_shifts = []
-        self.polynomials = {}
-
-        self.R = inputRanges[name]
-
-        if self.R[1] - self.R[0] < 0:
-            self.inverted = True
-            self.inversion = -1
-
-        self.scaling = 1.
-        if name == 'CH1':
-            self.gainEnabled = True
-            self.gainPGA = 1
-            self.gain = 0  # This is not the gain factor. use self.gain_values[self.gain] to find that.
-        elif name == 'CH2':
-            self.gainEnabled = True
-            self.gainPGA = 2
-            self.gain = 0
+        if name == "CH1":
+            self.programmable_gain_amplifier = 1
+        elif name == "CH2":
+            self.programmable_gain_amplifier = 2
         else:
-            pass
+            self.programmable_gain_amplifier = None
 
-        self.gain = 0
-        self.regenerateCalibration()
+        self.cal_poly10 = np.poly1d([3.3 / RESOLUTION_10BIT, 0])
+        self.cal_poly12 = np.poly1d([3.3 / RESOLUTION_12BIT, 0])
+        self.calibration_ready = False
+        self.chosa = PIC_ADC_MULTIPLEX[self.name]
+        self.adc_shifts = []
+        self.polynomials = []
+        self.gain_idx = 0
+        self.reset_calibration()
 
-    def setGain(self, g):
-        if not self.gainEnabled:
-            print('Analog gain is not available on', self.name)
-            return False
-        self.gain = self.gain_values.index(g)
-        self.regenerateCalibration()
+        # Backwards compatibility
+        self.gainPGA = self.programmable_gain_amplifier
+        self.CHOSA = self.chosa
+        self.calPoly10 = self.cal_poly10
+        self.calPoly12 = self.cal_poly12
+        self.calibrationReady = self.calibration_ready
+        self.regenerateCalibration = self.reset_calibration
+        self.setGain = self.set_gain
 
-    def inRange(self, val):
-        v = self.voltToCode12(val)
-        return 0 <= v <= 4095
+    def set_gain(self, g):
+        if self.name not in ("CH1", "CH2"):
+            raise RuntimeError(f"Analog gain is not available on {self.name}")
+        self.gain_idx = GAIN_VALUES.index(g)
+        self.reset_calibration()
 
-    def __conservativeInRange__(self, val):
-        v = self.voltToCode12(val)
-        return 50 <= v <= 4000
-
-    def loadCalibrationTable(self, table, slope, intercept):
+    def load_calibration_table(self, table, slope, intercept):
         self.adc_shifts = np.array(table) * slope - intercept
 
-    def __ignoreCalibration__(self):
-        self.calibrationReady = False
+    def _ignore_calibration(self):
+        self.calibration_ready = False
 
-    def loadPolynomials(self, polys):
-        for a in range(len(polys)):
-            epoly = [float(b) for b in polys[a]]
-            self.polynomials[a] = np.poly1d(epoly)
+    def load_polynomials(self, polys):
+        self.polynomials = []
+        for p in polys:
+            epoly = [float(b) for b in p]
+            self.polynomials.append(np.poly1d(epoly))
 
-    def regenerateCalibration(self):
-        B = self.R[1]
-        A = self.R[0]
-        intercept = self.R[0]
+    def reset_calibration(self):
+        B = INPUT_RANGES[self.name][1]
+        A = INPUT_RANGES[self.name][0]
 
-        if self.gain is not None:
-            gain = self.gain_values[self.gain]
+        if self.gain_idx is not None:
+            gain = GAIN_VALUES[self.gain_idx]
             B /= gain
             A /= gain
 
         slope = B - A
         intercept = A
-        if self.calibrationReady and self.gain != 8:  # special case for 1/11. gain
-            self.calPoly10 = self.__cal10__
-            self.calPoly12 = self.__cal12__
 
-        else:
-            self.calPoly10 = np.poly1d([0, slope / 1023., intercept])
-            self.calPoly12 = np.poly1d([0, slope / 4095., intercept])
+        if self.calibration_ready and self.gain_idx != 8:
+            self.cal_poly10 = self._cal10
+            self.cal_poly12 = self._cal12
+        else:  # special case for 1/11 gain
+            self.cal_poly10 = np.poly1d([slope / RESOLUTION_10BIT, intercept])
+            self.cal_poly12 = np.poly1d([slope / RESOLUTION_12BIT, intercept])
 
-        self.voltToCode10 = np.poly1d([0, 1023. / slope, -1023 * intercept / slope])
-        self.voltToCode12 = np.poly1d([0, 4095. / slope, -4095 * intercept / slope])
+        self.volt2code10 = np.poly1d(
+            [RESOLUTION_10BIT / slope, -RESOLUTION_10BIT * intercept / slope]
+        )
+        self.voltToCode10 = self.volt2code10  # Backwards compatibility
+        self.volt2code12 = np.poly1d(
+            [RESOLUTION_12BIT / slope, -RESOLUTION_12BIT * intercept / slope]
+        )
+        self.voltToCode12 = self.volt2code12  # Backwards compatibility
 
-    def __cal12__(self, RAW):
-        avg_shifts = (self.adc_shifts[np.int16(np.floor(RAW))] + self.adc_shifts[np.int16(np.ceil(RAW))]) / 2.
-        RAW -= 4095 * avg_shifts / 3.3
-        return self.polynomials[self.gain](RAW)
+    def _cal12(self, raw):
+        avg_shifts = (
+            self.adc_shifts[np.floor(raw)] + self.adc_shifts[np.ceil(raw)]
+        ) / 2  # Interpolate instead?
+        raw -= RESOLUTION_12BIT * avg_shifts / 3.3
 
-    def __cal10__(self, RAW):
-        RAW *= 4095 / 1023.
-        avg_shifts = (self.adc_shifts[np.int16(np.floor(RAW))] + self.adc_shifts[np.int16(np.ceil(RAW))]) / 2.
-        RAW -= 4095 * avg_shifts / 3.3
-        return self.polynomials[self.gain](RAW)
+        return self.polynomials[self.gain_idx](raw)
 
+    def _cal10(self, raw):
+        raw *= RESOLUTION_12BIT / RESOLUTION_10BIT
+        avg_shifts = (
+            self.adc_shifts[np.floor(raw)] + self.adc_shifts[np.ceil(raw)]
+        ) / 2  # Interpolate instead?
+        raw -= RESOLUTION_12BIT * avg_shifts / 3.3
 
-'''
-for a in ['CH1']:
-	x=analogInputSource(a)
-	print (x.name,x.calPoly10#,calfacs[x.name][0])
-	print ('CAL:',x.calPoly10(0),x.calPoly10(1023))
-	x.setOffset(1.65)
-	x.setGain(32)
-	print (x.name,x.calPoly10#,calfacs[x.name][0])
-	print ('CAL:',x.calPoly10(0),x.calPoly10(1023))
-'''
-
-
-# ---------------------------------------------------------------------
+        return self.polynomials[self.gain_idx](raw)
 
 
-class analogAcquisitionChannel:
+analogInputSource = AnalogInputSource  # Backwards compatibility
+
+
+class AnalogAcquisitionChannel(AnalogInputSource):
+    """Takes care of oscilloscope data fetched from the device.
+
+    Each instance may be linked to a particular input. Since only up to two channels
+    may be captured at a time with the PSLab, only two instances will be required.
+
+    Each instance will be linked to a particular inputSource instance by the capture
+    routines. When data is requested , it will return after applying calibration and
+    gain details stored in the selected inputSource.
     """
-    This class takes care of oscilloscope data fetched from the device.
-    Each instance may be linked to a particular input.
-    Since only up to two channels may be captured at a time with the PSLab, only two instances will be required
 
-    Each instance will be linked to a particular inputSource instance by the capture routines.
-    When data is requested , it will return after applying calibration and gain details
-    stored in the selected inputSource
-    """
-
-    def __init__(self, a):
-        self.name = ''
-        self.gain = 0
-        self.channel = a
-        self.channel_names = allAnalogChannels
-        # REFERENCE VOLTAGE = 3.3 V
-        self.calibration_ref196 = 1.  # measured reference voltage/3.3
-        self.resolution = 10
-        self.xaxis = np.zeros(10000)
-        self.yaxis = np.zeros(10000)
-        self.length = 100
-        self.timebase = 1.
-        self.source = analogInputSource('CH1')  # use CH1 for initialization. It will be overwritten by set_params
+    def __init__(
+        self,
+        channel: str,
+        resolution: int = 10,
+        length: int = 100,
+        timebase: float = 1.0,
+    ):
+        self.resolution = resolution
+        self.length = length
+        self.timebase = timebase
+        self._xaxis = np.zeros(10000)
+        self._yaxis = np.zeros(10000)
+        self._update_xaxis()
+        super().__init__(channel)
 
     def fix_value(self, val):
-        # val[val>1020]=np.NaN
-        # val[val<2]=np.NaN
         if self.resolution == 12:
-            return self.calibration_ref196 * self.source.calPoly12(val)
+            return self.cal_poly12(val)
         else:
-            return self.calibration_ref196 * self.source.calPoly10(val)
+            return self.cal_poly10(val)
 
-    def set_yval(self, pos, val):
-        self.yaxis[pos] = self.fix_value(val)
+    @property
+    def xaxis(self) -> np.ndarray:
+        return self._xaxis[: self.length]
+
+    @xaxis.setter
+    def xaxis(self, value: np.ndarray):
+        self._xaxis[: self.length] = value
+
+    def _update_xaxis(self):
+        self.xaxis = self.timebase * np.arange(self.length)
+
+    @property
+    def yaxis(self):
+        return self._yaxis[: self.length]
+
+    @yaxis.setter
+    def yaxis(self, value):
+        self._yaxis[: self.length] = value
+
+    # Backwards compatibility
+    def set_params(self, channel, resolution, length, timebase, **keys):
+        if channel != self.name:
+            self.__init__(channel, resolution, length, timebase)
+        else:
+            self.resolution = resolution
+            self.length = length
+            self.timebase = timebase
+            self._update_xaxis()
 
     def set_xval(self, pos, val):
         self.xaxis[pos] = val
 
-    def set_params(self, **keys):
-        self.gain = keys.get('gain', self.gain)
-        self.name = keys.get('channel', self.channel)
-        self.source = keys.get('source', self.source)
-        self.resolution = keys.get('resolution', self.resolution)
-        l = keys.get('length', self.length)
-        t = keys.get('timebase', self.timebase)
-        if t != self.timebase or l != self.length:
-            self.timebase = t
-            self.length = l
-            self.regenerate_xaxis()
-
-    def regenerate_xaxis(self):
-        for a in range(int(self.length)): self.xaxis[a] = self.timebase * a
+    def set_yval(self, pos, val):
+        self.yaxis[pos] = self.fix_value(val)
 
     def get_xaxis(self):
-        return self.xaxis[:int(self.length)]
+        return self.xaxis
 
     def get_yaxis(self):
-        return self.yaxis[:int(self.length)]
+        return self.yaxis
+
+
+analogAcquisitionChannel = AnalogAcquisitionChannel  # Backwards compatibility
