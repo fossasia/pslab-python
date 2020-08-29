2a21f6f0cc50c524dacc586f8b2b50a0d10e4216
Alexander Bessman <alexander@bessman.se>
2020-08-29 13:29:28 +0200

    Workaround firmware trigger bug

--- PSL/digital_channel.py
+++ PSL/digital_channel.py
@@ -4,11 +4,11 @@ DIGITAL_INPUTS = ('ID1', 'ID2', 'ID3', 'ID4', 'SEN', 'EXT', 'CNTR')
 digital_channel_names = DIGITAL_INPUTS
 
 MODES = {
-    "every sixteenth rising edge": 5,
-    "every fourth rising edge": 4,
-    "every rising edge": 3,
-    "every falling edge": 2,
-    "every edge": 1,
+    "sixteen rising": 5,
+    "four rising": 4,
+    "rising": 3,
+    "falling": 2,
+    "any": 1,
     "disabled": 0,
 }
 
@@ -20,7 +20,7 @@ class DigitalInput:
         self.datatype = "long"
         self.events_in_buffer = 0
         self.buffer_idx = None
-        self.logic_mode = MODES["every edge"]
+        self.logic_mode = MODES["any"]
 
     def xy(self, initial_state: bool, timestamps: np.ndarray):
         x = np.repeat(timestamps, 3)
@@ -30,13 +30,13 @@ class DigitalInput:
 
         if self.logic_mode == MODES["disabled"]:
             y[:] = initial_state
-        elif self.logic_mode == MODES["every edge"]:
+        elif self.logic_mode == MODES["any"]:
             y[0] = initial_state
             for i in range(1, len(x), 3):
                 y[i] = y[i - 1]  # Value before this timestamp.
                 y[i + 1] = not y[i]  # Value at this timestamp.
                 y[i + 2] = y[i + 1]  # Value leaving this timetamp.
-        elif self.logic_mode == MODES["every falling edge"]:
+        elif self.logic_mode == MODES["falling"]:
             y[0] = True
             for i in range(1, len(x), 3):
                 y[i] = True  # Value before this timestamp.
