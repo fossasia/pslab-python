6c7c902b0f56c88c6e508377301b953711a6c9f3
Alexander Bessman <alexander.bessman@scania.com>
2020-10-08 14:24:26 +0200

    Don't install udev rules automatically

--- setup.py
+++ setup.py
@@ -1,85 +1,21 @@
-from distutils.cmd import Command
-from distutils.util import execute
-import os
-import platform
-import shutil
-from subprocess import call
-import warnings
-
-from setuptools import setup, find_packages
-from setuptools.command.develop import develop
-from setuptools.command.install import install
+import setuptools
 
 
-def udev_reload_rules():
-    call(["udevadm", "control", "--reload-rules"])
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-
-def udev_trigger():
-    call(  # nosec
-        [
-            "udevadm",
-            "trigger",
-            "--subsystem-match=usb",
-            "--attr-match=idVendor=04d8",
-            "--action=add",
-        ]
-    )
-
-
-def install_udev_rules():
-    shutil.copy("99-pslab.rules", "/lib/udev/rules.d")
-    execute(udev_reload_rules, [], "Reloading udev rules")
-    execute(udev_trigger, [], "Triggering udev rules")
-
-
-def check_root():
-    return os.geteuid() == 0
-
-
-class CustomInstall(install):
-    def run(self):
-        install.run(self)
-        self.run_command("udev")
-
-
-class CustomDevelop(develop):
-    def run(self):
-        develop.run(self)
-        try:
-            self.run_command("udev")
-        except OSError as e:
-            warnings.warn(e)
-
-
-class InstallUdevRules(Command):
-    description = "install udev rules (requires root privileges)."
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        if platform.system() == "Linux":
-            if check_root():
-                install_udev_rules()
-            else:
-                msg = "You must have root privileges to install udev rules."
-                raise OSError(msg)
-
-
-setup(
-    name="PSL",
-    version="1.1.0",
+setuptools.setup(
+    name="pslab",
+    version="2.0.0.dev2",
     description="Pocket Science Lab by FOSSASIA",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="FOSSASIA PSLab Developers",
     author_email="pslab-fossasia@googlegroups.com",
     url="https://pslab.io/",
     install_requires=["numpy>=1.16.3", "pyserial>=3.4", "scipy>=1.3.0"],
-    packages=find_packages(exclude=("tests",)),
+    python_requires=">=3.6",
+    packages=setuptools.find_packages(exclude=("tests",)),
     package_data={
         "": [
             "*.css",
@@ -95,9 +31,10 @@ setup(
             "99-pslab.rules",
         ]
     },
-    cmdclass={
-        "develop": CustomDevelop,
-        "install": CustomInstall,
-        "udev": InstallUdevRules,
-    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+    ],
 )
