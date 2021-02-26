8a6dea5234c39d8305043b24580a41350ca1ff5c
Alexander Bessman <alexander@bessman.se>
2021-02-26 21:14:20 +0100

    Release v2.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pslab",
-    version="2.0.1",
+    version="2.1.0",
     description="Pocket Science Lab by FOSSASIA",
     long_description=long_description,
     long_description_content_type="text/markdown",
