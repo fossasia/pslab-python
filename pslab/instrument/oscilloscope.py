3f3e51912ed3e433d74af25bd891feae40824586
Shubham Kushwaha <65639964+EricLiclair@users.noreply.github.com>
2021-03-15 18:53:18 +0530

    Set gain before capturing trace (fix #171)

--- pslab/instrument/oscilloscope.py
+++ pslab/instrument/oscilloscope.py
@@ -132,6 +132,11 @@ class Oscilloscope(ADCBufferMixin):
 
         self._check_args(channels, samples, timegap)
         timegap = int(timegap * 8) / 8
+
+        for channel in ("CH1", "CH2"):
+            # Reset gain (another instance could have changed it).
+            self._set_gain(channel, self._channels[channel].gain)
+
         self._capture(channels, samples, timegap)
         x = [timegap * np.arange(samples)]
 
