455f7d0c7bed47a5cd70d8cbbbb4f38f634c391b
Daniel Maslowski <info@orangecms.org>
2021-05-22 16:51:57 +0200

    MLX90614: migrate to new I2C API

--- pslab/external/MLX90614.py
+++ pslab/external/MLX90614.py
@@ -1,46 +1,40 @@
-from __future__ import print_function
+from pslab.bus import I2CSlave
 
-
-def connect(route, **args):
-    return MLX90614(route, **args)
-
-
-class MLX90614():
+class MLX90614(I2CSlave):
+    _ADDRESS = 0x5A
+    _OBJADDR = 0x07
+    _AMBADDR = 0x06
     NUMPLOTS = 1
     PLOTNAMES = ['Temp']
-    ADDRESS = 0x5A
     name = 'PIR temperature'
 
-    def __init__(self, I2C, **args):
-        self.I2C = I2C
-        self.ADDRESS = args.get('address', self.ADDRESS)
-        self.OBJADDR = 0x07
-        self.AMBADDR = 0x06
+    def __init__(self):
+        super().__init__(self._ADDRESS)
 
-        self.source = self.OBJADDR
+        self.source = self._OBJADDR
 
         self.name = 'Passive IR temperature sensor'
         self.params = {'readReg': {'dataType': 'integer', 'min': 0, 'max': 0x20, 'prefix': 'Addr: '},
                        'select_source': ['object temperature', 'ambient temperature']}
 
-        try:
-            print('switching baud to 100k')
-            self.I2C.configI2C(100e3)
-        except Exception as e:
-            print('FAILED TO CHANGE BAUD RATE', e.message)
+        # try:
+        #     print('switching baud to 100k')
+        #     self.I2C.configI2C(100e3)
+        # except Exception as e:
+        #     print('FAILED TO CHANGE BAUD RATE', e.message)
 
     def select_source(self, source):
         if source == 'object temperature':
-            self.source = self.OBJADDR
+            self.source = self._OBJADDR
         elif source == 'ambient temperature':
-            self.source = self.AMBADDR
+            self.source = self._AMBADDR
 
     def readReg(self, addr):
         x = self.getVals(addr, 2)
         print(hex(addr), hex(x[0] | (x[1] << 8)))
 
     def getVals(self, addr, numbytes):
-        vals = self.I2C.readBulk(self.ADDRESS, addr, numbytes)
+        vals = self.read(numbytes, addr)
         return vals
 
     def getRaw(self):
@@ -54,7 +48,7 @@ class MLX90614():
             return False
 
     def getObjectTemperature(self):
-        self.source = self.OBJADDR
+        self.source = self._OBJADDR
         val = self.getRaw()
         if val:
             return val[0]
@@ -62,7 +56,7 @@ class MLX90614():
             return False
 
     def getAmbientTemperature(self):
-        self.source = self.AMBADDR
+        self.source = self._AMBADDR
         val = self.getRaw()
         if val:
             return val[0]
