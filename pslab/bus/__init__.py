8d447633ae16ab6782a67f773380e7aab785e527
nkpro2000sr <srnaveen2k@yahoo.com>
2021-07-07 01:35:42 +0530

    Refactor SPI

--- pslab/bus/__init__.py
+++ pslab/bus/__init__.py
@@ -1,11 +1,14 @@
 """Contains modules for interfacing with the PSLab's I2C, SPI, and UART buses.
 
-(SPI and UART still TODO)
+(UART still TODO)
 """
 
 from pslab.bus.i2c import I2CMaster, I2CSlave
+from pslab.bus.spi import SPIMaster, SPISlave
 
 __all__ = (
     "I2CMaster",
     "I2CSlave",
+    "SPIMaster",
+    "SPISlave",
 )
