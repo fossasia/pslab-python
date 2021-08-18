7824e6bdb5342fb4afb0101c82a95b4c3589f456
nkpro2000sr <srnaveen2k@yahoo.com>
2021-08-18 17:49:56 +0530

    Add test_uart

---
+++ tests/test_uart.py
@@ -0,0 +1,94 @@
+"""Tests for pslab.bus.uart.
+
+When integration testing, the PSLab's logic analyzer is used to verify the
+function of the I2C bus. Before running the integration tests, connect:
+    TxD2->LA1
+    RxD2->SQ1
+"""
+
+import pytest
+
+from pslab.bus.uart import UART
+from pslab.instrument.logic_analyzer import LogicAnalyzer
+from pslab.instrument.waveform_generator import PWMGenerator
+from pslab.serial_handler import SerialHandler, MockHandler
+
+SPI_SUPPORTED_DEVICES = [
+    # "PSLab vMOCK",  # Uncomment after adding recording json files.
+    "PSLab V6",
+]
+
+WRITE_DATA = 0x55
+TXD2 = "LA1"
+RXD2 = "SQ1"
+PWM_FERQUENCY = UART._baudrate
+MICROSECONDS = 1e-6
+RELTOL = 0.05
+# Number of expected logic level changes.
+TXD_START = 1
+TXD_WRITE_DATA = 8 # if LSB is 1
+TXD_STOP = 1  # if data MSB is 0
+
+
+@pytest.fixture
+def uart(handler: SerialHandler) -> UART:
+    if handler.version not in SPI_SUPPORTED_DEVICES:
+        pytest.skip("UART2 not supported by this device.")
+    handler._logging = True
+    return UART(device=handler)
+
+
+@pytest.fixture
+def la(handler: SerialHandler) -> LogicAnalyzer:
+    handler._logging = True
+    return LogicAnalyzer(handler)
+
+
+@pytest.fixture
+def pwm(handler: SerialHandler) -> PWMGenerator:
+    if not isinstance(handler, MockHandler):
+        pwm = PWMGenerator(handler)
+        pwm.generate(RXD2, PWM_FERQUENCY, 0.5)
+        handler._logging = True
+
+
+def test_configure(la: LogicAnalyzer, uart: UART):
+    baudrate = 1000000
+    uart.configure(baudrate)
+    la.capture(1, block=False)
+    uart.write_byte(WRITE_DATA)
+    la.stop()
+    (txd2,) = la.fetch_data()
+    start_to_stop = 1 + 8 + 1
+    period = (txd2[-1] - txd2[0]) / start_to_stop
+    assert (period * MICROSECONDS) ** -1 == pytest.approx(baudrate, rel=RELTOL)
+
+
+def test_write_byte(la: LogicAnalyzer, uart: UART):
+    la.capture(3, block=False)
+    uart.write_byte(WRITE_DATA)
+    la.stop()
+    (txd2,) = la.fetch_data()
+
+    assert len(txd2) == (TXD_START + TXD_WRITE_DATA + TXD_STOP)
+
+
+def test_write_int(la: LogicAnalyzer, uart: UART):
+    la.capture(3, block=False)
+    uart.write_int((WRITE_DATA << 8) | WRITE_DATA)
+    la.stop()
+    (txd2,) = la.fetch_data()
+
+    assert len(txd2) == 2 * (TXD_START + TXD_WRITE_DATA + TXD_STOP)
+
+
+def test_read_byte(pwm: PWMGenerator, uart: UART):
+    value = uart.read_byte()
+
+    assert value in (0, 0xFF)
+
+
+def test_read_int(pwm: PWMGenerator, uart: UART):
+    value = uart.read_int()
+
+    assert value in (0, 0xFFFF)
