c6996e9ef9a0b738b7d0b729fb31aaae396b7c15
Alexander Bessman <alexander@bessman.se>
2024-07-02 12:38:31 +0200

    Update power_supply tests

--- tests/test_power_supply.py
+++ tests/test_power_supply.py
@@ -7,6 +7,9 @@ When running integration tests, connect:
     PCS -> 100R -> GND
     PCS -> CH3
 """
+
+import time
+
 import pytest
 import numpy as np
 
@@ -14,8 +17,8 @@ from pslab.serial_handler import SerialHandler
 from pslab.instrument.multimeter import Multimeter
 from pslab.instrument.power_supply import PowerSupply
 
-RELTOL = 0.05
-ABSTOL = 0.05
+RELTOL = 0.1
+ABSTOL = 0.1
 
 
 @pytest.fixture
@@ -36,6 +39,7 @@ def test_set_voltage_pv1(power: PowerSupply, multi: Multimeter):
 
     for i, v in enumerate(voltages):
         power.pv1 = v
+        time.sleep(0.01)
         measured[i] = multi.measure_voltage("CH1")
 
     assert measured == pytest.approx(voltages, rel=RELTOL * 2, abs=ABSTOL * 2)
@@ -47,6 +51,7 @@ def test_set_voltage_pv2(power: PowerSupply, multi: Multimeter):
 
     for i, v in enumerate(voltages):
         power.pv2 = v
+        time.sleep(0.01)
         measured[i] = multi.measure_voltage("CH2")
 
     assert measured == pytest.approx(voltages, rel=RELTOL * 2, abs=ABSTOL * 2)
@@ -58,17 +63,19 @@ def test_set_voltage_pv3(power: PowerSupply, multi: Multimeter):
 
     for i, v in enumerate(voltages):
         power.pv3 = v
+        time.sleep(0.01)
         measured[i] = multi.measure_voltage("VOL")
 
     assert measured == pytest.approx(voltages, rel=RELTOL * 2, abs=ABSTOL * 2)
 
 
 def test_set_current(power: PowerSupply, multi: Multimeter):
-    currents = np.arange(0, 3e-3, 1e-4)
+    currents = np.arange(0, 2.5e-3, 1e-4)
     measured = np.zeros(len(currents))
 
     for i, c in enumerate(currents):
         power.pcs = c
+        time.sleep(0.01)
         measured[i] = multi.measure_voltage("CH3")
 
     resistor = 100
